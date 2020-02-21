5b1391be8a906078fe829cb1b61dfc9b09e28131
Nginx web server <nginx@mail.refulgent.de>
2020-02-21 23:39:06 +0100

    Korrigiert Namespaces

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,7 +5,7 @@
 /**
  * Frontend modules
  */
-$GLOBALS['FE_MOD']['user'][\Refulgent\ContaoLDAPSupportBundle\Legacy\Ldap\Ldap::MODULE_LDAP_LOGIN] = 'HeimrichHannot\Ldap\ModuleLdapLogin';
+$GLOBALS['FE_MOD']['user'][\Refulgent\ContaoLDAPSupportBundle\Ldap::MODULE_LDAP_LOGIN] = 'HeimrichHannot\Ldap\ModuleLdapLogin';
 
 /**
  * Hooks
@@ -13,13 +13,13 @@ $GLOBALS['FE_MOD']['user'][\Refulgent\ContaoLDAPSupportBundle\Legacy\Ldap\Ldap::
 if (TL_MODE == 'FE' && \Config::get('addLdapForMembers'))
 {
     // order is correct
-    $GLOBALS['TL_HOOKS']['importUser'][]       = ['Refulgent\ContaoLDAPSupportBundle\Legacy\Ldap\Backend\LdapMember', 'importPersonFromLdap'];
-    $GLOBALS['TL_HOOKS']['checkCredentials'][] = ['Refulgent\ContaoLDAPSupportBundle\Legacy\Ldap\Backend\LdapMember', 'authenticateAgainstLdap'];
+    $GLOBALS['TL_HOOKS']['importUser'][]       = ['Refulgent\ContaoLDAPSupportBundle\Backend\LdapMember', 'importPersonFromLdap'];
+    $GLOBALS['TL_HOOKS']['checkCredentials'][] = ['Refulgent\ContaoLDAPSupportBundle\Backend\LdapMember', 'authenticateAgainstLdap'];
 }
 
 if (TL_MODE == 'BE' && \Config::get('addLdapForUsers'))
 {
     // order is correct
-    $GLOBALS['TL_HOOKS']['importUser'][]       = ['Refulgent\ContaoLDAPSupportBundle\Legacy\Ldap\Backend\LdapUser', 'importPersonFromLdap'];
-    $GLOBALS['TL_HOOKS']['checkCredentials'][] = ['Refulgent\ContaoLDAPSupportBundle\Legacy\Ldap\Backend\LdapUser', 'authenticateAgainstLdap'];
+    $GLOBALS['TL_HOOKS']['importUser'][]       = ['Refulgent\ContaoLDAPSupportBundle\Backend\LdapUser', 'importPersonFromLdap'];
+    $GLOBALS['TL_HOOKS']['checkCredentials'][] = ['Refulgent\ContaoLDAPSupportBundle\Backend\LdapUser', 'authenticateAgainstLdap'];
 }
